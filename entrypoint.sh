d2ccfacb95d74300e913a0995cf1966f1ec8d40d
Dominic <chinonsodomnic@gmail.com>
2024-07-28 22:10:26 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ PATH="/srv/hngprojects"
 CONTAINER_NAME="${BRANCH_NAME}_${PR_NUMBER}_container"
 echo "Starting deployment..."
 
-apt install sshpass
+# apt install sshpass
 sshpass -p $SERVER_PASSWORD ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST << EOF
   set -e
 
